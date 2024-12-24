bfbee3be50d836da346654deb9171ab7b1d030a2
devsafix <devsafix@gmail.com>
2024-12-24 21:18:46 +0600

    Learn navigation using Link component and useRouter hook

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,32 @@
+"use client"
 import Image from "next/image";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+
+  const router = useRouter();
+  console.log(router);
+
+  const handleRoute = () => {
+    router.push("/account");
+  }
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <h1>Welcome to next js</h1>
+    <div className="flex items-center justify-center gap-5 py-20">
+
+      <Link href={"/products"} className="p-2 bg-blue-500 text-white rounded">
+        Navigate to product page
+      </Link>
+      <Link href={"/account"} className="p-2 bg-pink-500 text-white rounded">
+        Navigate to account page
+      </Link>
+
+
+      <h1 onClick={handleRoute} className="p-2 bg-purple-500 text-white rounded">
+        Navigate with alternative way
+      </h1>
+
     </div>
   );
 }
